b27b8999573adb3b3e9ef446f8314be82ffab4cd
Ansh goyal <56803262+coder-cop@users.noreply.github.com>
2020-08-06 22:44:24 +0530

    Add files via upload

---
+++ svm/predict.py
@@ -0,0 +1,58 @@
+import numpy as np
+import csv
+import sys
+import pickle
+import sklearn
+from sklearn.svm import SVC
+
+from validate import validate
+
+
+"""
+Predicts the target values for data in the file at 'test_X_file_path', using the model learned during training.
+Writes the predicted values to the file named "predicted_test_Y_svm.csv". It should be created in the same directory where this code file is present.
+This code is provided to help you get started and is NOT a complete implementation. Modify it based on the requirements of the project.
+"""
+
+def import_data_and_model(test_X_file_path, model_file_path):
+    test_X = np.genfromtxt(test_X_file_path, delimiter=',', dtype=np.float64, skip_header=1)
+    model = pickle.load(open(model_file_path, 'rb'))
+    return test_X, model
+
+
+def predict_target_values(test_X, model):
+    # Write your code to Predict Target Variables
+    # HINT: You can use other functions which you've already implemented in coding assignments.
+    mean = np.mean(test_X,0)
+    std = np.std(test_X,0)
+    test_X -= mean
+    test_X /= std
+    Y = model.predict(test_X)
+    Y= Y.tolist()
+    Y=[Y]
+    Y=np.asarray(Y)
+    Y=(Y.T).tolist()
+
+    return Y
+
+
+def write_to_csv_file(pred_Y, predicted_Y_file_name):
+    pred_Y = np.array(pred_Y)
+    pred_Y = pred_Y.reshape(len(pred_Y),1)
+    with open(predicted_Y_file_name, 'w', newline='') as csv_file:
+        wr = csv.writer(csv_file)
+        wr.writerows(pred_Y)
+        csv_file.close()
+
+
+def predict(test_X_file_path):
+    test_X, model = import_data_and_model(test_X_file_path, 'MODEL_FILE1.sav')
+    pred_Y = predict_target_values(test_X, model)
+    write_to_csv_file(pred_Y, "predicted_test_Y_svm.csv")    
+
+
+if __name__ == "__main__":
+    test_X_file_path = sys.argv[1]
+    predict(test_X_file_path)
+    # Uncomment to test on the training data
+    # validate(test_X_file_path, actual_test_Y_file_path="train_Y_svm.csv") 
